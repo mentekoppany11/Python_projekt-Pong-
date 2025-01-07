6ddc9df3b9d23cf961b4dc43252ebcc6678d5dcc
Mente_Koppány <koppany.mente@gmail.com>
2025-01-07 17:10:10 +0100

    labda finomítása

--- djad.py
+++ djad.py
@@ -1,13 +1,22 @@
+import time
 import pygame
 from sys import exit
 
 
+
 pygame.init()
+
 screen = pygame.display.set_mode((1000,600))
+
 pygame.display.set_caption("Pong")
-pygame.display.set_icon('')
+icon_surf = pygame.image.load('C:\\Users\\Koppany\\Desktop\\beadando\\icon.png')
+pygame.display.set_icon(icon_surf)
+
 clock = pygame.time.Clock()
-score_font = pygame.font.Font('game_font.ttf')
+
+
+
+score_font = pygame.font.Font('game_font.ttf',"""size""")
 
 
 
@@ -17,14 +26,13 @@ BLUE       = (0, 0, 255)
 RED        = (255, 0, 0)
 ORANGE     = (255, 165, 0)
 
-#
+
 
 
 
 player1_surf = pygame.Surface((30, 200))
 player1_surf.fill(RED)
 player1_rect = player1_surf.get_rect(midleft = (50, 300))
-
 
 player2_surf = pygame.Surface((30, 200))
 player2_surf.fill(BLUE)
@@ -37,12 +45,8 @@ ball_rect = ball_surf.get_rect(center = (500, 300))
 ball_speed_x = 6
 ball_speed_y = 6
 
-
 player1_score = 0
 player2_score = 0
-
-score_surf = score_font.render(f'Player1 {player1_score}:{player2_score} Player2', False, LIGHT_GRAY)
-score_rect = score_surf.get_rect(center = (500, 25))
 
 while True:
     for event in pygame.event.get():
@@ -50,6 +54,9 @@ while True:
             pygame.quit()
             exit()
 
+    score_surf = score_font.render(f'Player1 {player1_score}:{player2_score} Player2', False, LIGHT_GRAY)
+    score_rect = score_surf.get_rect(center = (500, 25))
+    
     screen.fill(GRAY)
 
     screen.blit(player1_surf, player1_rect)  
@@ -76,15 +83,32 @@ while True:
     if ball_rect.top <= 0 or ball_rect.bottom >= 600:
         ball_speed_y *= -1
 
-    if ball_rect.right <= 0:
+    if ball_rect.left >= 1010:
+       #labda vissza helyezése középre
         ball_rect.x = 500
         ball_rect.y = 300
+
+       #Játékos2 pontja növelése
         player1_score += 1
 
-    if ball_rect.left >= 1000:
+       #labda sebességének a növelése
+        ball_speed_x *= -1.05 #labda irányának a meg fordítása
+        ball_speed_y *= 1.05
+
+        
+
+
+    if ball_rect.right <= -10:
+       #labda vissza helyezése középre
         ball_rect.x = 500
         ball_rect.y = 300
+       #Játékos2 pontja növelése
         player2_score += 1
+       #labda sebességének a növelése
+        ball_speed_x *= -1.05 #labda irányának a meg fordítása
+        ball_speed_y *= 1.05
+
+        
 
     if player1_rect.colliderect(ball_rect) or player2_rect.colliderect(ball_rect):
         ball_speed_x *= -1
